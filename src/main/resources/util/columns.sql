db06fb79ed25676f1d1862c298022f0969f864c5
evertonbrunosds <evertonbrunosds@gmail.com>
2026-04-14 05:27:23 +0000

    fix: validação de email por constraint a nível de banco

--- src/main/resources/util/columns.sql
+++ src/main/resources/util/columns.sql
@@ -1,1 +1,3 @@
 ALTER TABLE profile ADD COLUMN IF NOT EXISTS name_purified CHARACTER VARYING(30) GENERATED ALWAYS AS (purify(name)::text) STORED;
+ALTER TABLE profile DROP CONSTRAINT IF EXISTS profile_email_format_check;
+ALTER TABLE profile ADD CONSTRAINT profile_email_format_check CHECK (email::text ~ '^[a-z0-9._%+-]+@(?![.-])[a-z0-9.-]*[a-z0-9](?<!-)(?<!\.)[.][a-z]{2,}$'::text);
