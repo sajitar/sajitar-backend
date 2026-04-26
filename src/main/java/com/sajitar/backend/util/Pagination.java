c4e0e3a57ea821408d6d89fbbcdda1b806cee746
evertonbrunosds <evertonbrunosds@gmail.com>
2026-04-26 01:48:03 -0300

    refactor: simplificando a paginação

--- src/main/java/com/sajitar/backend/util/Pagination.java
+++ src/main/java/com/sajitar/backend/util/Pagination.java
@@ -1,35 +1,20 @@
 package com.sajitar.backend.util;
 
 import java.util.List;
+import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Builder;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.With;
 
 @With
 @Builder(toBuilder = true)
-@RequiredArgsConstructor
-public class Pagination<T> {
-
-    @Getter(onMethod_ = @JsonProperty(index = 0))
-    private final List<T> content;
-
-    @Getter(onMethod_ = @JsonProperty(index = 1))
-    private final long precedingElements;
-
-    @Getter(onMethod_ = @JsonProperty(index = 2))
-    private final long followingElements;
-
-    @Getter(onMethod_ = @JsonProperty(index = 3))
-    private final boolean reverse;
+public record Pagination<T>(List<T> content, long precedingElements, long followingElements, boolean reverse) {
 
     @JsonIgnore
     public boolean isEmpty() {
-        return content == null;
+        return Objects.isNull(content);
     }
 
 }
