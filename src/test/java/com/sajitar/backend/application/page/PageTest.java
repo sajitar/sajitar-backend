00abb313dc5bfdc4156a64506450f54832b5d624
evertonbrunosds <evertonbrunosds@gmail.com>
2026-09-02 02:16:46 -0300

    feat: add sajitar-backend architecture and implementation details with validation and testing guidelines

--- src/test/java/com/sajitar/backend/application/page/PageTest.java
+++ src/test/java/com/sajitar/backend/application/page/PageTest.java
@@ -2,8 +2,6 @@ package com.sajitar.backend.application.page;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
